12139dde7ab53d4e2e70a74a4c0d53903621df2f
ANUBHAV KAUSHIK <anubhavgreen41@gmail.com>
2018-07-26 22:22:09 +0530

    Scala implementation of reversed linked list
    Scala implementation of reversed linked list

---
+++ ReverseLinkList.scala
@@ -0,0 +1,63 @@
+object ReverseLinkedList{
+
+  def main(args: Array[String]){
+
+    var head = Some(new Node(2))
+    head.get.nxt = Some(new Node(3))
+    head.get.nxt.get.nxt= Some(new Node(5))
+    printNodes(head)
+    var newHead = reverseAndPrintRecursion(head,None)
+    printf("-------")
+    printNodes(newHead)
+    newHead = reverseAndPrint(newHead)
+    printf("s"+newHead.get.num)
+    printNodes(newHead)
+
+  }
+  def printNodes( head:Option[Node]):Unit =
+  {
+    head match
+    {
+      case None => print("......finished")
+      case x => { print("  "+x.get.num); printNodes(x.get.nxt) }
+
+    }
+  }
+
+  def reverseAndPrintRecursion(head:Option[Node], prvNode:Option[Node]):Option[Node]=
+  {
+    var newHead = Option(new Node(0))
+    head match
+    {
+      case None => return prvNode
+      case x => newHead = reverseAndPrintRecursion(x.get.nxt,x)
+    }
+    prvNode match
+    {
+      case None => head.get.nxt=None
+      case x => head.get.nxt=prvNode
+    }
+      return newHead
+
+  }
+
+  def reverseAndPrint(head:Option[Node]):Option[Node]=
+  {
+     var current =head
+     var prev :Option[Node] = None
+     var nxt : Option[Node] = head.get.nxt
+     head.get.nxt = None
+     while(current!=None){
+        if(prev !=None){
+            nxt = current.get.nxt
+            current.get.nxt = prev
+        }
+       prev = current
+       current =nxt
+     }
+
+    return prev
+  }
+
+  class Node(var num:Int, var nxt:Option[Node]=None)
+}
